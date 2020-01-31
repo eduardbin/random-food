6943147eb21687a8e64c0dacb55859595b652876
eduardbin <binukeduard@gmail.com>
2020-01-31 13:00:21 +0200

    add data with one restaurant of european food and some code for the second

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -18,14 +18,44 @@ import './lib/slick.min.js';
 $(document).foundation();
 
 
-// 'use strict';
+'use strict';
+//Create and functional map in index-page
+let deliveryMap;
 
-// let deliveryMap;
+window.addEventListener('load', initDeliveryMap);
 
-// document.addEventListener('load', initDeliveryMap);
-// function initDeliveryMap() {
-// 	deliveryMap = new google.maps.Map(document.querySelector('.ed-delivery__map'), {
-// 		center: { lat: -34.397, lng: 150.644 },
-// 		zoom: 8
-// 	});
-// };
+function initDeliveryMap() {
+	// The location of mapCenter
+	let cities = {
+		poltava: { lat: 49.592232, lng: 34.545601 },
+	};
+
+	let mapCenter = cities.poltava;
+
+	//map-options
+	deliveryMap = new google.maps.Map(document.querySelector('.ed-delivery__map'), {
+		center: mapCenter,
+		zoom: 12,
+		disableDefaultUI: true,
+	});
+
+	//move mapCenter on select change
+	// let citySelect = document.querySelector('*input-select-data-atribute*');
+
+	// citySelect.addEventListener('change', () => {
+	// 	let city = citySelect.value;
+	// 	let newCoords = cities[city];
+
+	// 	deliveryMap.setCenter(newCoords);
+	// });
+
+	//try map with geolocation while we dont use city-list in header or
+	navigator.geolocation.getCurrentPosition(function (position) {
+		var pos = {
+			lat: position.coords.latitude,
+			lng: position.coords.longitude
+		};
+
+		deliveryMap.setCenter(pos);
+	});
+};
